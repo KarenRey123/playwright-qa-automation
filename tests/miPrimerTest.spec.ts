1c2079cca9fc6b6adb5bd2c29dcf83da56786e1d
Karen Xiomara <kxra4688@gmail.com>
2026-08-20 16:59:27 -0500

    Add SauceDemo title test

--- tests/miPrimerTest.spec.ts
+++ tests/miPrimerTest.spec.ts
@@ -1,20 +1,6 @@
 import { test, expect } from '@playwright/test';
 
-test('Login exitoso en SauceDemo', async ({ page }) => {
-
-  // 1. Abrir la aplicación
+test('verificar título de SauceDemo', async ({ page }) => {
   await page.goto('https://www.saucedemo.com/');
-
-  // 2. Ingresar usuario
-  await page.locator('[data-test="username"]').fill('standard_user');
-
-  // 3. Ingresar contraseña
-  await page.locator('[data-test="password"]').fill('secret_sauce');
-
-  // 4. Hacer clic en Login
-  await page.locator('[data-test="login-button"]').click();
-
-  // 5. Validar que el login fue exitoso
-  await expect(page.getByText('Products')).toBeVisible();
-
+  await expect(page).toHaveTitle(/Swag Labs/);
 });
